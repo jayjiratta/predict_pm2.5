cfaf3d1d11cec1a536e57be7d483966e918c46a2
jayjiratta <151592814+jayjiratta@users.noreply.github.com>
2024-02-29 09:37:25 +0700

    value error

--- air4thai.py
+++ air4thai.py
@@ -11,11 +11,11 @@ end_date = "2024-02-20"
 start_time = "00"
 end_time = "23"
 
-for _ in station_id:
-    url = f"http://air4thai.com/forweb/getHistoryData.php?stationID={station_id}&param={param}&type={data_type}&sdate={start_date}&edate={end_date}&stime={start_time}&etime={end_time}"
+for i in station_id:
+    url = f"http://air4thai.com/forweb/getHistoryData.php?stationID={i}&param={param}&type={data_type}&sdate={start_date}&edate={end_date}&stime={start_time}&etime={end_time}"
     response = requests.get(url)
     response_json = response.json()
 
     pd_from_dict = pd.DataFrame.from_dict(response_json["stations"][0]["data"])
     print(pformat(pd_from_dict))
-    pd_from_dict.to_csv(f"air4thai_{station_id}_{start_date}_{end_date}.csv")
+    pd_from_dict.to_csv(f"air4thai_{i}_{start_date}_{end_date}.csv")
