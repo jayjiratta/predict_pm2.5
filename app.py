7390db7ad158a8af3f25e70047f3232874c7e1fd
Chaapppp <chanisarappp@gmail.com>
2024-03-01 11:22:28 +0700

    Split DATETIMEDATA

--- app.py
+++ app.py
@@ -12,6 +12,10 @@ df1 = df[df.isnull().sum(axis=1) < 3]
 cl_null = ['PM25', 'PM10', 'O3', 'CO', 'NO2', 'SO2', 'WS', 'TEMP', 'RH', 'WD']
 df2 = df1[cl_null] = df1[cl_null].fillna(df1[cl_null].mean().round(2))
 
+df3 = df2.join(df[['DATETIMEDATA']].set_axis(df.index))
+df3[['Date', 'Time']] = df3['DATETIMEDATA'].str.split(' ', expand=True)
+df4=df3.drop('DATETIMEDATA', axis=1)
+
 # data["Date"] = pd.to_datetime(data["Date"], format="%Y-%m-%d")
 # data.sort_values("Date", inplace=True)
 
