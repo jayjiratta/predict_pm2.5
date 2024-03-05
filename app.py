e71d0683666ba44224a9016b6fdabc3ac40a8924
Chaapppp <chanisarappp@gmail.com>
2024-03-05 12:50:58 +0700

    Add template

--- app.py
+++ app.py
@@ -26,6 +26,26 @@ app = Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 app.title = "Avocado Analytics: Understand Your Avocados!"
 
+template = html.Div(
+    children=[
+        html.Div(
+            children=[
+                html.P(children="🥑", className="header-emoji"),
+                html.H1(
+                    children="Avocado Analytics", className="header-title"
+                ),
+                html.P(
+                    children="Analyze the behavior of avocado prices"
+                    " and the number of avocados sold in the US"
+                    " between 2015 and 2018",
+                    className="header-description",
+                ),
+            ],
+            className="header",
+        ),
+    ]
+)
+
 app.layout = html.Div(
     children=[
         html.Div(
