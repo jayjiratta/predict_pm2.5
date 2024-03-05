a96df0ea1290a77490d04e7a34dff1daac39ca82
Chaapppp <chanisarappp@gmail.com>
2024-03-05 12:52:28 +0700

    Add home layout

--- app.py
+++ app.py
@@ -46,23 +46,9 @@ template = html.Div(
     ]
 )
 
-app.layout = html.Div(
+layout_home = html.Div(
     children=[
-        html.Div(
-            children=[
-                html.P(children="🥑", className="header-emoji"),
-                html.H1(
-                    children="Avocado Analytics", className="header-title"
-                ),
-                html.P(
-                    children="Analyze the behavior of avocado prices"
-                    " and the number of avocados sold in the US"
-                    " between 2015 and 2018",
-                    className="header-description",
-                ),
-            ],
-            className="header",
-        ),
+        template,
         html.Div(
             children=[
                 html.Div(
